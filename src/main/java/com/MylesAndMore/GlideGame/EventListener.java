9549f4e9ab97b1bd3fb0f913a9373e7214e2a714
Myles <43725835+MylesAndMore@users.noreply.github.com>
2023-02-14 20:55:15 +0000

    do we even need a listener anyways?

---
+++ src/main/java/com/MylesAndMore/GlideGame/EventListener.java
@@ -0,0 +1,11 @@
+package com.MylesAndMore.GlideGame;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+/**
+ * GlideGame event listener for all plugin and game-related events.
+ */
+public class EventListener implements Listener {
+    
+}
